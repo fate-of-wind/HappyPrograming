49449a09c0c163e6a97acf7eaf506d25dbe9ab56
yuhuan12 <yuhuan12@students.cs.ubc.ca>
2021-02-08 21:40:28 +0800

    fix some bug in task 4 Finish a part of implement of user story and Test those method. - part of task 4 in phase 1

--- src/main/model/Hand.java
+++ src/main/model/Hand.java
@@ -2,7 +2,6 @@ package model;
 // Hand is hand card of each player. At first, each Hand contains 15 cards. - 5 rock, 5 paper, and 5 scissors
 
 public class Hand {
-    private int num;
     Card rockCard = new Card("rock");
     Card paperCard = new Card("paper");
     Card scissorCard = new Card("scissor");
@@ -36,9 +35,9 @@ public class Hand {
     //MODIFIES:this
     //EFFECTS: remove one card of specific kind, so number of this kind minus 1
     public void removeSpecificOneCard(Card card) {
-        if (card.getKinds() == rockCard.getKinds()) {
+        if (card.getKinds().equals(rockCard.getKinds())) {
             this.rock.removeOneCard();
-        } else if (card.getKinds() == paperCard.getKinds()) {
+        } else if (card.getKinds().equals(paperCard.getKinds())) {
             this.paper.removeOneCard();
         } else {
             this.scissor.removeOneCard();
@@ -48,9 +47,9 @@ public class Hand {
     //MODIFIES:this
     //EFFECTS: add one card of specific kind, so number of this kind minus 1
     public void addSpecificOneCard(Card card) {
-        if (card.getKinds() == rockCard.getKinds()) {
+        if (card.getKinds().equals(rockCard.getKinds())) {
             this.rock.addOneCard();
-        } else if (card.getKinds() == paperCard.getKinds()) {
+        } else if (card.getKinds().equals(paperCard.getKinds())) {
             this.paper.addOneCard();
         } else {
             this.scissor.addOneCard();
