e6e2adbaf661c1a5ea0ceca958cabcb3fd2bfb23
yuhuan12 <yuhuan12@students.cs.ubc.ca>
2021-03-20 07:34:45 +0800

    (build basic frame)need to fix bug on Icon

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -17,16 +17,23 @@ import javax.swing.Box;
 public class GUI extends JFrame implements ActionListener {
     public GUI() {
         JFrame frame = new JFrame("Card Game");
+        frame.setLayout(new BorderLayout());
+        Box b1 = Box.createHorizontalBox();
+        Box b2 = Box.createVerticalBox();
         JPanel panel = new JPanel();
         JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+        panel3.add(b1);
         panel.setBackground(Color.WHITE);
 
         JButton rock = new JButton();
         JButton paper = new JButton("paper");
         JButton scissor = new JButton("scissor");
 
-        ImageIcon rockIcon = new ImageIcon(getClass().getResource("./data/rock.png"));
-        rock.setIcon(rockIcon);
+        //ImageIcon rockIcon = new ImageIcon(getClass().getResource("./data/rock.png"));
+        //rock.setIcon(rockIcon);
         Dimension cardSize = new Dimension(200,300);
         rock.setPreferredSize(cardSize);
         paper.setPreferredSize(cardSize);
@@ -40,15 +47,21 @@ public class GUI extends JFrame implements ActionListener {
         paper.addActionListener(this);
 
         panel.setBackground(Color.YELLOW);
-        panel2.setBackground(Color.BLUE);
-        panel2.setPreferredSize(new Dimension(600,300));
+        panel4.setBackground(Color.BLUE);
+        panel4.setPreferredSize(new Dimension(600,300));
+        panel5.setBackground(Color.BLACK);
+        panel5.setPreferredSize(new Dimension(300,300));
 
-        panel.add(panel2);
-        //panel.add(rock);
-        //panel.add(scissor);
-        //panel.add(paper);
+
+        panel3.add(panel4);
+        b1.add(Box.createHorizontalStrut(270));
+        panel3.add(panel5);
+        panel2.add(rock);
+        panel2.add(scissor);
+        panel2.add(paper);
         frame.add(panel);
-        //frame.add(panel2);
+        frame.add(panel2,BorderLayout.SOUTH);
+        frame.add(panel3,BorderLayout.NORTH);
         frame.setBounds(1900,1000,1900,1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
